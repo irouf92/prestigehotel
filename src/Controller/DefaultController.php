6753ea2dd0db3dfca1be0f3fd8ab8b49e3591c99
NessrineBekh <nessrine.bekhtaoui@gmail.com>
2022-09-14 14:15:08 +0200

    form contact + page non edité presentation hotel

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,17 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function home(): Response
     {
-        return $this->render('default/home.html.twig', [
-           
-        ]);
+        return $this->render('default/home.html.twig');
     }
 }
