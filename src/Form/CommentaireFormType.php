9ef8b8759783a35cbc606f589cb336f3bdde31bd
NessrineBekh <nessrine.bekhtaoui@gmail.com>
2022-09-15 09:24:36 +0200

    updated Form et view twig

--- src/Form/CommentaireFormType.php
+++ src/Form/CommentaireFormType.php
@@ -20,84 +20,82 @@ class CommentaireFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom', TextType::class,[
-            'label' => 'Nom',
-            'constraints' => [
-                new NotBlank([
-                    'message' => 'Ce champs ne peut être vide.'
-                ]),
-            ]
+            ->add('nom', TextType::class, [
+                'label' => 'Nom',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Ce champs ne peut être vide.'
+                    ]),
+                ]
             ])
             ->add('prenom', TextType::class, [
-            'label' => 'Prénom',
-            'constraints' => [
-                new NotBlank([
-                    'message' => 'Ce champs ne peut être vide.'
-                ]),
-            ]
-        ])
+                'label' => 'Prénom',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Ce champs ne peut être vide.'
+                    ]),
+                ]
+            ])
             ->add('telephone', TextType::class, [
-            'label' => 'Téléphone',
-            'constraints' => [
-                new NotBlank([
-                    'message' => 'Ce champs ne peut être vide.'
-                ]),
-            ]
-        ])
-            ->add('email',EmailType::class, [
-            'label' => 'Votre email',
-            'constraints' => [
-                new NotBlank([
-                    'message' => 'Ce champs ne peut être vide.'
-                ]),
-                new Email([
-                    'message' => "l'email n'est pas au bon format ! Ex : mail@example.com"
-                ]),
-                new Length([
-                    'min' => 4,
-                    'max' => 255
-                ])
-            ]
-        ])
-            
-            ->add('titre', TextType::class, [
-            'label' => 'Titre',
-            'constraints' => [
-                new NotBlank([
-                    'message' => 'Ce champs ne peut être vide.'
-                ]),
-            ]
-        ])
-
+                'label' => 'Téléphone',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Ce champs ne peut être vide.'
+                    ]),
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Votre email',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Ce champs ne peut être vide.'
+                    ]),
+                    new Email([
+                        'message' => "l'email n'est pas au bon format ! Ex : mail@example.com"
+                    ]),
+                    new Length([
+                        'min' => 4,
+                        'max' => 255
+                    ])
+                ]
+            ])
             ->add('categorie', ChoiceType::class, [
-            'label' => 'Catégorie',
-            
-            'choices' => [
-                'Hotel' => 'hotel',
-                'Chambre' => 'chambre',
-                'Restaurant' => 'restaurant',
-                'Spa' => 'spa',
-                'Service' => 'service',
-                
-            ],
-        ])
-        ->add('commentaire', TextareaType::class, [
-            'attr' => ['rows' => 5],
-            'constraints' => [
-                new NotBlank([
-                    'message' => 'Ce champs ne peut être vide.'
-                ]),
-            ]
-        ])
-            ->add('submit', SubmitType::class, [
-            'label' => "Envoyer",
-            'validate' => false,
-            'attr' => [
-                'class' => 'd-block mx-auto mmy-3 col-4 btn btn-dark'
-            ]
-        ])
+                'label' => 'Sujet',
 
-        ;
+                'choices' => [
+                    'Hotel' => 'hotel',
+                    'Chambre' => 'chambre',
+                    'Restaurant' => 'restaurant',
+                    'Spa' => 'spa',
+                    'Service' => 'service',
+
+                ],
+            ])
+            ->add('titre', TextType::class, [
+                'label' => 'Titre',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Ce champs ne peut être vide.'
+                    ]),
+                ]
+            ])
+
+
+            ->add('commentaire', TextareaType::class, [
+                'attr' => ['rows' => 5],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Ce champs ne peut être vide.'
+                    ]),
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "Envoyer",
+                'validate' => false,
+                'attr' => [
+                    'class' => 'd-block mx-auto mmy-3 col-4 btn btn-dark'
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
